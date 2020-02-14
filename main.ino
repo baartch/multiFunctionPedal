0f28d95e14bc7b96be4c6721497e71a8850b61a1
Andy Brunner <abrunner@baart.ch>
2020-02-14 15:06:43 +0100

    tuner funktionert mit tft

--- main.ino
+++ main.ino
@@ -1,16 +1,18 @@
-// Adafruit GFX Library - Version: Latest
+#include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SPITFT.h>
-#include <Adafruit_SPITFT_Macros.h>
-#include <gfxfont.h>
-
-
-// Adafruit SSD1306 - Version: 2.1.0
 #include <Adafruit_SSD1306.h>
-#include <splash.h>
-
 // eigene Programm Dateien
 #include "tuner.h"
+
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+extern Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 #define MODE 1 // 0 = COMPRESSOR / 1 = TUNER
 
@@ -18,18 +20,9 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma region DISPLAY
 // DISPLAY 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#pragma endregion
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma region TUNER variables
-// TUNER
 
 #pragma endregion
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // FUNCTIONS
@@ -40,7 +33,7 @@ void setup()
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   //Set the text size and color.
   display.setTextSize(1);
-  display.setTextColor(WHITE);
+  display.setTextColor(SSD1306_WHITE);
 }
 
 
@@ -53,9 +46,9 @@ void setup()
 void loop()
 {
   //Clear previous image.
-  display.clearDisplay();
 
   if (MODE == 0){ //COMPRESSOR
+    display.clearDisplay();
     byte poti_threshold = (analogRead(A1)/4);
     byte poti_ratio = (analogRead(A2)/4);
 
@@ -63,15 +56,30 @@ void loop()
       drawCompSettings(poti_threshold,poti_ratio);
     }
 
+    display.display();
     delay(100);
   }
   
-  display.setCursor(0, 10);
+  //display.setCursor(0, 10);
 
-  if (MODE == 1){ //TUNER
-    tuner();
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // TUNER
+  if (MODE == 1){
+    if ((micros() - startzeit) >= messzeit)
+    {
+      display.clearDisplay();
+      float f = timer; //Datentyp 'float', wegen untenstehender Division
+      f = 1000000/f; //Aus Periodendauer Frequenz berechnen
+      detachInterrupt(0);
+      display.setCursor(0, 10);
+      display.print(f);
+      display.display();
+
+      attachInterrupt(digitalPinToInterrupt(2), Messung, RISING);
+      zaehler = 0; //Frequenzzähler zurücksetzen
+      startzeit = micros(); //Zeitpunkt der letzten Ausgabe speichern
+    }
   }
-
 
 
 
@@ -94,12 +102,12 @@ void drawCompSettings (byte thresh, float ratio) {
   // draw Threshhold
   display.setCursor(SCREEN_WIDTH/2, 10);
   display.print(thresh);
-  display.fillTriangle(SCREEN_HEIGHT, SCREEN_HEIGHT, threshX, threshY, threshX, SCREEN_HEIGHT, WHITE);
-  display.fillRect(threshX, threshY, (128-threshX), (SCREEN_HEIGHT-threshY), WHITE);
+  display.fillTriangle(SCREEN_HEIGHT, SCREEN_HEIGHT, threshX, threshY, threshX, SCREEN_HEIGHT, SSD1306_WHITE);
+  display.fillRect(threshX, threshY, (128-threshX), (SCREEN_HEIGHT-threshY), SSD1306_WHITE);
 
   //calculate ratio
   ratio = (((float)ratio*19)/potiMax)+1; // Die 20 ist das Maximum aus dem Compressor Script // 19+1 damit der Minimal-Wert 1 ist
   display.setCursor(SCREEN_WIDTH/2, 20);
   display.print(ratio);
-  display.fillTriangle(threshX+1 , threshY-1, SCREEN_WIDTH, (threshY-((float)threshY/ratio)), SCREEN_WIDTH, threshY-1, WHITE);
+  display.fillTriangle(threshX+1 , threshY-1, SCREEN_WIDTH, (threshY-((float)threshY/ratio)), SCREEN_WIDTH, threshY-1, SSD1306_WHITE);
 }
--- tuner.cpp
+++ tuner.cpp
@@ -10,19 +10,7 @@ unsigned long messzeit = 1000000;
 
 
 void tuner (){
-      if ((micros() - startzeit) >= messzeit)
-    {
-        float f = timer; //Datentyp 'float', wegen untenstehender Division
-        f = 1000000/f; //Aus Periodendauer Frequenz berechnen
-        detachInterrupt(0);
 
-            Serial.print("Messung: ");
-            Serial.println(f, 1);
-
-        attachInterrupt(digitalPinToInterrupt(2), Messung, RISING);
-        zaehler = 0; //Frequenzzähler zurücksetzen
-        startzeit = micros(); //Zeitpunkt der letzten Ausgabe speichern
-    }
 }
 
 
